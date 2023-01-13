cbfbcddf2744a35ca5d82f8469108f84648fd285
Julien Lefebvre <julienlefebvre399@gmail.com>
2023-01-13 14:24:58 +0100

    Projet

---
+++ ExerciceTD1A/app/src/main/java/fr/lefebvre/exercicetd1a/IncidentPage.java
@@ -0,0 +1,14 @@
+package fr.lefebvre.exercicetd1a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IncidentPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incident_page);
+    }
+}
